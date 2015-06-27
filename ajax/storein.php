fd7f67777dc4d79d5c6c333cc4c83e29b4814bba
Sawyer-007 <sawyer_007@outlook.com>
2015-06-28 01:37:05 +0800

    no bug

--- ajax/storein.php
+++ ajax/storein.php
@@ -32,24 +32,52 @@
             <div class="modal-content">
                 <div class="modal-header">
                     <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
-                    <h4 class="modal-title">添加入库信息</h4>
+                    <h4 class="modal-title">添加人员信息</h4>
                 </div>
-                <form method="post" action="data/storeinins.php" target="_blank">
+                <form method="post" action="data/storeinins.php" class="form-horizontal" target="_blank">
                     <div class="modal-body">
-                        <label>装备编号</label>
-                        <input type="text" id="inputzbid" name="zbid" class="form-control" placeholder="Example:0001" required>
-                        <label>生产日期</label>
-                        <input type="text" id="inputmakedata" name="makedate" class="form-control" placeholder="Example:20150101" required>
-                        <label>装备单价</label>
-                        <input type="text" id="inputzbprice" name="zbprice" class="form-control" placeholder="Example:1000000000" required>
-                        <label>装备数量</label>
-                        <input type="text" id="inputzbnum" name="zbnum" class="form-control" placeholder="Example:1" required>
-                        <label>仓库编号</label>
-                        <input type="text" id="inputSid" name="sid" class="form-control" placeholder="Example:00001" required>
-                        <label>验收人</label>
-                        <input type="text" id="inputryname1" name="ryname1" class="form-control" placeholder="Example:张三" required>
-                        <label>备注</label>
-                        <input type="text" id="inputmemo" name="memo" class="form-control" placeholder="Example:＝。＝" required>
+                        <div class="form-group">
+                            <label for="inputzbid" class="col-sm-4 control-label">装备编号</label>
+                            <div class="col-sm-8">
+                                <input type="text" id="inputzbid" name="zbid" class="form-control" placeholder="Example:0001" required autofocus >
+                            </div>
+                        </div>
+                        <div class="form-group">
+                            <label for="inputmakedata" class="col-sm-4 control-label">生产日期</label>
+                            <div class="col-sm-8">
+                                <input type="text" id="inputmakedata" name="makedate" class="form-control" placeholder="Example:20150101" required>
+                            </div>
+                        </div>
+                        <div class="form-group">
+                            <label for="inputzbprice" class="col-sm-4 control-label">装备单价</label>
+                            <div class="col-sm-8">
+                                <input type="text" id="inputzbprice" name="zbprice" class="form-control" placeholder="Example:1000000000" required>
+                            </div>
+                        </div>
+                        <div class="form-group">
+                            <label for="inputzbnum" class="col-sm-4 control-label">装备数量</label>
+                            <div class="col-sm-8">
+                                <input type="text" id="inputzbnum" name="zbnum" class="form-control" placeholder="Example:1" required>
+                            </div>
+                        </div>
+                        <div class="form-group">
+                            <label for="inputSid" class="col-sm-4 control-label">仓库编号</label>
+                            <div class="col-sm-8">
+                                <input type="text" id="inputSid" name="sid" class="form-control" placeholder="Example:00001" required>
+                            </div>
+                        </div>
+                        <div class="form-group">
+                            <label for="inputryname1" class="col-sm-4 control-label">验收人</label>
+                            <div class="col-sm-8">
+                                <input type="text" id="inputryname1" name="ryname1" class="form-control" placeholder="Example:张三" required>
+                            </div>
+                        </div>
+                        <div class="form-group">
+                            <label for="inputmemo" class="col-sm-4 control-label">备注</label>
+                            <div class="col-sm-8">
+                                <input type="text" id="inputmemo" name="memo" class="form-control" placeholder="Example:＝。＝" required>
+                            </div>
+                        </div>   
                     </div>
                     <div class="modal-footer">
                         <input class="btn btn-lg btn-block btn-primary" type="submit" value="Add in">
